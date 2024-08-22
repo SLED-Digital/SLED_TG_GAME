b074f4652a3ffd0ab10f8086179e3299670858b6
hi@sledd.ru <hi@sledd.ru>
2024-08-22 21:41:27 +0300

    Start edit game

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-   const path = require('path');
-
-   module.exports = {
-       entry: './src/vi', // Входной файл твоего проекта
-       output: {
-           filename: 'bundle.js', // Имя файла сборки
-           path: path.resolve(__dirname, 'public') // Путь до папки public
-       },
-       mode: 'production', // Для минимизации используй 'production' режим
-       module: {
-           rules: [
-               {
-                   test: /\.js$/,
-                   exclude: /node_modules/,
-                   use: {
-                       loader: 'babel-loader',
-                       options: {
-                           presets: ['@babel/preset-env']
-                       }
-                   }
-               }
-           ]
-       }
-   };
+   // const path = require('path');
+   //
+   // module.exports = {
+   //     entry: './src/vi', // Входной файл твоего проекта
+   //     output: {
+   //         filename: 'bundle.js', // Имя файла сборки
+   //         path: path.resolve(__dirname, 'public') // Путь до папки public
+   //     },
+   //     mode: 'production', // Для минимизации используй 'production' режим
+   //     module: {
+   //         rules: [
+   //             {
+   //                 test: /\.js$/,
+   //                 exclude: /node_modules/,
+   //                 use: {
+   //                     loader: 'babel-loader',
+   //                     options: {
+   //                         presets: ['@babel/preset-env']
+   //                     }
+   //                 }
+   //             }
+   //         ]
+   //     }
+   // };
