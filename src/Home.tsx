8d887d33fca391f0bfd037b253bdd05e6787b1c8
iccc <hi@sledd.ru>
2024-08-23 03:16:07 +0300

    home_page готова

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,6 +7,7 @@ const Home = () => {
   const [energy, setEnergy] = useState(2532);
   const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
   const pointsToAdd = 12;
   const energyToReduce = 12;
   const navigate = useNavigate();
@@ -28,6 +29,7 @@ const Home = () => {
     updateUserInLocalStorage(newPoints, newEnergy);
 
     setClicks((prevClicks) => [...prevClicks, { id: Date.now(), x, y }]);
+    setIsClicked(true); // Устанавливаем состояние для анимации
   }, [energy, points, energyToReduce, pointsToAdd]);
 
   const handleAnimationEnd = useCallback((id: number) => {
@@ -98,6 +100,10 @@ const Home = () => {
     navigate(`${path}?telegramId=${telegramId}`);
   };
 
+  const handleWithdrawClick = () => {
+    navigate('/withdraw');
+  };
+
   if (!isAuthenticated) {
     return <div>Loading...</div>; // Или другая заглушка на время проверки авторизации
   }
@@ -115,89 +121,98 @@ const Home = () => {
 
   return (
     <div className="bg-gradient-main min-h-screen px-4 flex flex-col items-center text-white font-medium">
-      <div className="absolute inset-0 h-1/2 bg-gradient-overlay z-0"></div>
+  <div className="absolute inset-0 h-1/2 bg-gradient-overlay z-0"></div>
+
+      <div className="absolute bottom-0 w-full flex justify-center items-center pb-4">
+        <div className="buttonvivod-button" onClick={handleWithdrawClick}><p>вывести</p></div>
+      </div>
+
       <div className="absolute top background-kashtan flex flex-col items-center justify-center">
+    <div className="mt-12 text-5xl font-bold flex items-center margintext-top">
+      <img src={coin} width={39} height={44} />
+      <span className="ml-2 txt-size">{points.toLocaleString()}</span>
+    </div>
+  </div>
+  <div className="absolute top background-purple"></div>
 
-        <div className="mt-12 text-5xl font-bold flex items-center margintext-top">
-            <img src={coin} width={39} height={44} />
-            <span className="ml-2 txt-size">{points.toLocaleString()}</span>
+  <div className="absolute inset-0 flex items-center justify-center z-0">
+    <div className="radial-gradient-overlay"></div>
+  </div>
+
+  <div className="w-full z-10 min-h-screen flex flex-col items-center text-white">
+    <div className="fixed top-0 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white toolbar">
+      <div className="w-full flex justify-center items-center gap-2 mt-4">
+        <div className="block-energy">
+          <img src={highVoltage} width={44} height={44} alt="High Voltage" />
         </div>
-
-        <div className="absolute bottom-0 w-full flex justify-center items-center pb-4">
-            <div className="buttonvivod-button"><p>вывести</p></div>
-          </div>
+        <div className="w-full bg-[#f9c035] rounded-full mt-2 border-energy">
+          <div
+            className="h-4 rounded-full transition-all duration-500"
+            style={{
+              width: `${(energy / 500) * 100}%`,
+              backgroundColor: getEnergyColor(),
+            }}
+          ></div>
+        </div>
       </div>
-      <div className="absolute top background-purple"></div>
-
-      <div className="absolute inset-0 flex items-center justify-center z-0">
-        <div className="radial-gradient-overlay"></div>
-      </div>
-
-      <div className="w-full z-10 min-h-screen flex flex-col items-center text-white">
-        <div className="fixed top-0 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white toolbar">
-          <div className="w-full flex justify-center items-center gap-2 mt-4">
-            <div className="block-energy">
-              <img src={highVoltage} width={44} height={44} alt="High Voltage" />
-            </div>
-            <div className="w-full bg-[#f9c035] rounded-full mt-2 border-energy">
-              <div
-                className="h-4 rounded-full transition-all duration-500"
-                style={{
-                  width: `${(energy / 500) * 100}%`,
-                  backgroundColor: getEnergyColor(),
-                }}
-              ></div>
-            </div>
-          </div>
-          <div className="w-1/3 flex items-center justify-start max-w-32">
-            <div className="flex items-center justify-center">
-              <div className="ml-2 text-left farmer-number">
-                <span className="text-white text-2xl font-bold block textcolor-black">{energy}</span>
-                <span className="text-white text-large opacity-65 textcolor-black">/ 500</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex-grow flex items-center justify-center clicker-button relative">
-          <div className="relative mt-4 notc-block" onPointerDown={handlePointerDown}>
-            <img src={notcoin} width={528} height={528} alt="notcoin" className="notc-width" />
-            {clicks.map((click) => (
-              <div
-                key={click.id}
-                className="absolute text-5xl font-bold opacity-0"
-                style={{
-                  top: `${click.y - 21}px`, // Уменьшаем позицию, так как картинка будет меньше
-                  left: `${click.x - 14}px`, // Уменьшаем позицию, так как картинка будет меньше
-                  animation: `float 1s ease-out`
-                }}
-                onAnimationEnd={() => handleAnimationEnd(click.id)}
-              >
-                <img src={notcoin} width={56*0.8} height={56*0.8} alt="Coin" style={{ filter: 'invert(1)' }} />
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="fixed bottom-0 left-0 w-full px-3 pb-3 z-22 navigatblock">
-          <div className="w-full flex justify-between gap-2 navigat">
-            <div className="flex-grow flex items-center max-w-80 text-sm">
-              <div className="w-full bg-[#249D8C] py-4 rounded-2xl flex justify-around">
-                <button className="flex flex-col items-center gap-1" onClick={() => handleButtonClick('/earn')}>
-                  <img src={coin} width={24} height={24} alt="Earn" />
-                </button>
-                <button className="flex flex-col items-center gap-1" onClick={() => handleButtonClick('/frens')}>
-                  <img src={bear} width={24} height={24} alt="Frens" />
-                </button>
-                <button className="flex flex-col items-center gap-1" onClick={() => handleButtonClick('/boosts')}>
-                  <img src={rocket} width={24} height={24} alt="Boosts" />
-                </button>
-              </div>
-            </div>
+      <div className="w-1/3 flex items-center justify-start max-w-32">
+        <div className="flex items-center justify-center">
+          <div className="ml-2 text-left farmer-number">
+            <span className="text-white text-2xl font-bold block textcolor-black">{energy}</span>
+            <span className="text-white text-large opacity-65 textcolor-black">/ 500</span>
           </div>
         </div>
       </div>
     </div>
+
+    <div className="flex-grow flex items-center justify-center clicker-button relative">
+      <div className="relative mt-4 notc-block" onPointerDown={handlePointerDown}>
+        <div className="notc-click-area">
+          <img
+            src={notcoin}
+            width={528}
+            height={528}
+            alt="notcoin"
+            className={`notc-width ${isClicked ? 'click-animation' : ''}`}
+            onAnimationEnd={() => setIsClicked(false)}
+          />
+        </div>
+        {clicks.map((click) => (
+          <div
+            key={click.id}
+            className="absolute text-5xl font-bold opacity-0"
+            style={{
+              top: `${click.y - 21}px`, // Уменьшаем позицию, так как картинка будет меньше
+              left: `${click.x - 14}px`, // Уменьшаем позицию, так как картинка будет меньше
+              animation: `float 1s ease-out`
+            }}
+            onAnimationEnd={() => handleAnimationEnd(click.id)}
+          >
+            <img src={notcoin} width={56*0.8} height={56*0.8} alt="Coin" style={{ filter: 'invert(1)' }} />
+          </div>
+        ))}
+      </div>
+    </div>
+
+    <div className="fixed bottom-0 left-0 w-full px-4 pb-4 z-22 navigatblock">
+      <div className="w-full flex justify-between gap-2 navigat">
+        <div className="flex-grow flex items-center max-w-100 text-sm">
+          <div className="w-full bg-[#249D8C] py-4 rounded-2xl flex justify-around">
+            <button className="flex flex-col items-center gap-1" onClick={() => handleButtonClick('/earn')}>
+              <img src={coin} width={24} height={24} alt="Earn" />
+            </button>
+            <button className="flex flex-col items-center gap-1" onClick={() => handleButtonClick('/frens')}>
+              <img src={bear} width={24} height={24} alt="Frens" />
+            </button>
+            <button className="flex flex-col items-center gap-1" onClick={() => handleButtonClick('/boosts')}>
+              <img src={rocket} width={24} height={24} alt="Boosts" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   );
 };
 
