2836c6bd0fb18132c39248c1048a6fe5c09415bb
iccc <hi@sledd.ru>
2025-02-15 23:08:48 +0300

    start

--- src/Start.tsx
+++ src/Start.tsx
@@ -6,10 +6,10 @@ const BackgroundPage: React.FC = () => {
   return (
     <div className="background-page">
       <div className="content">
-        <video className="video" autoPlay muted loop>
-          <source src={bear} type="video/mp4" />
-          Ваш браузер не поддерживает элемент video.
-        </video>
+          <video className="video" autoPlay muted loop playsInline>
+              <source src={bear} type="video/mp4"/>
+              Ваш браузер не поддерживает элемент video.
+          </video>
       </div>
     </div>
   );
